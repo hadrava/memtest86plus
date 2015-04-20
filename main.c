mcb30/memtest

[pxe] Allow PXE NBP to accept a command line
Signed-off-by: Michael Brown <mcb30@ipxe.org>

main.c
@@ -143,19 +143,26 @@ void run_at(unsigned long addr)
 #define OLD_CL_MAGIC_ADDR ((unsigned short*) MK_PTR(INITSEG,0x20))
 #define OLD_CL_MAGIC 0xA33F 
 #define OLD_CL_OFFSET_ADDR ((unsigned short*) MK_PTR(INITSEG,0x22))
+#define PXE_CL_MAGIC_ADDR ((unsigned short*) MK_PTR(BOOTSEG,0x20))
+#define PXE_CL_OFFSET_ADDR ((unsigned short*) MK_PTR(BOOTSEG,0x22))
 
 static void parse_command_line(void)
 {
 	char *cmdline;
+	unsigned short offset;
 
 	if (cmdline_parsed)
 		return;
 
-	if (*OLD_CL_MAGIC_ADDR != OLD_CL_MAGIC)
+	if (*OLD_CL_MAGIC_ADDR == OLD_CL_MAGIC) {
+		offset = *OLD_CL_OFFSET_ADDR;
+		cmdline = MK_PTR(INITSEG, offset);
+	} else if (*PXE_CL_MAGIC_ADDR == OLD_CL_MAGIC) {
+		offset = *PXE_CL_OFFSET_ADDR;
+		cmdline = MK_PTR(BOOTSEG, offset);
+	} else {
 		return;
-
-	unsigned short offset = *OLD_CL_OFFSET_ADDR;
-	cmdline = MK_PTR(INITSEG, offset);
+	}
 
 	/* skip leading spaces */
 	while (*cmdline == ' ')

hadrava/memtest86plus

main.c
@@ -272,12 +272,15 @@ void reloc(void)
 #define OLD_CL_MAGIC_ADDR ((unsigned short*) MK_PTR(INITSEG,0x20))
 #define OLD_CL_MAGIC 0xA33F 
 #define OLD_CL_OFFSET_ADDR ((unsigned short*) MK_PTR(INITSEG,0x22))
+#define PXE_CL_MAGIC_ADDR ((unsigned short*) MK_PTR(BOOTSEG,0x20))
+#define PXE_CL_OFFSET_ADDR ((unsigned short*) MK_PTR(BOOTSEG,0x22))
 
 static void parse_command_line(void)
 {
 	long simple_strtoul(char *cmd, char *ptr, int base);
 	char *cp, dummy;
 	int i, j, k;
+	unsigned short offset;
 
 	if (cmdline_parsed)
 		return;
@@ -287,11 +290,15 @@ static void parse_command_line(void)
 		cpu_mask[i] = 1;
 	}
 
-	if (*OLD_CL_MAGIC_ADDR != OLD_CL_MAGIC)
+	if (*OLD_CL_MAGIC_ADDR == OLD_CL_MAGIC) {
+		offset = *OLD_CL_OFFSET_ADDR;
+		cp = MK_PTR(INITSEG, offset);
+	} else if (*PXE_CL_MAGIC_ADDR == OLD_CL_MAGIC) {
+		offset = *PXE_CL_OFFSET_ADDR;
+		cp = MK_PTR(BOOTSEG, offset);
+	} else {
 		return;
-
-	unsigned short offset = *OLD_CL_OFFSET_ADDR;
-	cp = MK_PTR(INITSEG, offset);
+	}
 
 	/* skip leading spaces */
 	while (*cp == ' ')